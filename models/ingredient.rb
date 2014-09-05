86ac6c54ecd3deb66cecfd5ed2db868db351987c
rachelbookhout <rbookhou@gmail.com>
2014-09-05 15:01:39 -0400

    Passed rspec

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,22 +1,12 @@
 require_relative '../server'
 
 class Ingredient
+
  attr_reader :name, :recipe_id
+
  def initialize(recipe_id,name)
   @recipe_id = recipe_id
   @name = name
-end
-
- def find_ingredients(recipe_id)
-@ingredients =[]
-food_list = db_connection do |conn|
-              conn.exec("SELECT * FROM ingredients WHERE recipes.id = $1", [id])
-              end
-food_list = food_list.to_a
-food_list.each do |food|
-    @ingredients << Ingredient.new(food["recipe_id"], food["name"])
-     end
-  @ingredients
-  end
+ end
 
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,8 +1,5 @@
 require_relative '../server'
 require_relative 'ingredient'
-require 'pry'
-require 'sinatra'
-require 'pg'
 
 class Recipe
   attr_reader :id, :name, :instructions, :description, :ingredients
@@ -18,57 +15,40 @@ class Recipe
 
 
 
-def self.all
-  @recipes = []
-  recipes = db_connection do |conn|
+  def self.all
+    @recipes = []
+    recipes = db_connection do |conn|
               conn.exec("SELECT * FROM recipes")
             end
-  recipes = recipes.to_a
-  recipes.each do |data|
+    recipes = recipes.to_a
+    recipes.each do |data|
      @recipes << Recipe.new(data["id"],data["name"],data["instructions"], data["description"])
-     end
-  @recipes
+    end
+    @recipes
   end
 
 
-def self.find_ingredients(id)
-@ingredients =[]
-food_list = db_connection do |conn|
+  def self.find_ingredients(id)
+  @ingredients =[]
+  food_list = db_connection do |conn|
               conn.exec("SELECT * FROM ingredients WHERE recipe_id = $1", [id])
               end
-food_list = food_list.to_a
-food_list.each do |food|
+  food_list = food_list.to_a
+  food_list.each do |food|
     @ingredients << Ingredient.new(food["recipe_id"], food["name"])
-     end
-  #binding.pry
+  end
   @ingredients
   end
 
 
-def self.find(id)
-#binding.pry
-info =  db_connection do |conn|
+  def self.find(id)
+  info =  db_connection do |conn|
             conn.exec("SELECT recipes.id,recipes.name,recipes.description, recipes.instructions FROM recipes
             WHERE recipes.id = $1", [id])
           end
- #binding.pry
- info = info.to_a
- #binding.pry
-  #binding.pry
+  info = info.to_a
   recipe_ing = find_ingredients(id)
-  #binding.pry
   @recipe = Recipe.new(info[0]["id"],info[0]["name"],info[0]["instructions"], info[0]["description"], recipe_ing)
-  #end
-  #binding.pry
-
-end
-
-
-
-
-
-
-
-
+  end
 
 end
